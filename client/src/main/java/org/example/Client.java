f268297ef0e97d1a485f9c1dc33aee80843e887c
sean xiao <435032183@qq.com>
2023-11-29 11:11:04 +0800

    reference: https://blog.csdn.net/friendlytkyj/article/details/128889875

---
+++ client/src/main/java/org/example/Client.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Client {
+    public static void main(String[] args) {
+        SpringApplication.run(Client.class, args);
+    }
+}
---
+++ client/src/main/java/org/example/Oauth2ClientAutoConfiguration.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class Oauth2ClientAutoConfiguration {
+
+    @Bean
+    public SecurityFilterChain authorizationClientSecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests()
+                .anyRequest().authenticated()
+                .and().logout()
+                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
+                .and().oauth2Client()
+                .and().oauth2Login();
+
+        return http.build();
+    }
+}
+
