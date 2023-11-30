59a63f028a49faa8543892291534839a2d693d5a
sean xiao <435032183@qq.com>
2023-11-30 14:40:42 +0800

    can get authentication in client

--- client/src/main/java/org/example/ClientController.java
+++ client/src/main/java/org/example/ClientController.java
@@ -2,6 +2,13 @@ package org.example;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.oidc.OidcIdToken;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,13 +17,31 @@ import org.springframework.web.bind.annotation.RestController;
 public class ClientController {
 
     @Autowired
-    public WelcomeClient welcomeClient;
+    private WelcomeClient welcomeClient;
+    @Autowired
+    private OAuth2AuthorizedClientService oAuth2AuthorizedClientService;
 
     @GetMapping("/client")
-    public String welcome() {
-
+    public String welcome(Authentication authentication) {
+        String authorities = authentication.getName() + " - " + authentication.getAuthorities().toString();
         String welcome = welcomeClient.getWelcome();
-        return "<h1>" +  welcome + "</h1>";
+        return "<h1>" +  welcome + "</h1><h2>" + authorities + "</h2>";
+    }
+    @GetMapping("/token")
+    public String token(Authentication authentication) {
+        OAuth2AuthenticationToken oAuth2AuthenticationToken = (OAuth2AuthenticationToken) authentication;
+        OAuth2AuthorizedClient oAuth2AuthorizedClient = oAuth2AuthorizedClientService
+                .loadAuthorizedClient(oAuth2AuthenticationToken.getAuthorizedClientRegistrationId(), oAuth2AuthenticationToken.getName());
+        String jwtAccessToken = oAuth2AuthorizedClient.getAccessToken().getTokenValue();
+        String jwtRefrechToken = oAuth2AuthorizedClient.getRefreshToken().getTokenValue();
+        return "<b>JWT Access Token: </b>" + jwtAccessToken + "<br/><br/><b>JWT Refresh Token:  </b>" + jwtRefrechToken;
+    }
+
+    @GetMapping("idtoken")
+    public String idtoken(@AuthenticationPrincipal OidcUser oidcUser) {
+        OidcIdToken oidcIdToken = oidcUser.getIdToken();
+        String idTokenValue = oidcIdToken.getTokenValue();
+        return "<b>Id Token: </b>" + idTokenValue;
     }
 }
 
--- client/src/main/java/org/example/SecurityConfig.java
+++ client/src/main/java/org/example/SecurityConfig.java
@@ -2,25 +2,89 @@ package org.example;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
+import com.nimbusds.jwt.JWT;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.JWTParser;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizationRequestResolver;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestCustomizers;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestRedirectFilter;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.text.ParseException;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 @Configuration
 public class SecurityConfig {
 
+//    @Bean
+//    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                //所有请求都需经过授权认证
+//                .authorizeHttpRequests(authorize -> authorize
+//                        .anyRequest().authenticated())
+//                //配置登录URL
+//                .oauth2Login(oauth2Login ->
+//                        oauth2Login.loginPage("/oauth2/authorization/myoauth2"))
+//                //使用默认客户端配置
+//                .oauth2Client(withDefaults());
+//        return http.build();
+//    }
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    SecurityFilterChain securityFilterChain(HttpSecurity http, ClientRegistrationRepository clientRegistrationRepository) throws Exception {
+
+        String base_uri = OAuth2AuthorizationRequestRedirectFilter.DEFAULT_AUTHORIZATION_REQUEST_BASE_URI;
+        DefaultOAuth2AuthorizationRequestResolver resolver = new DefaultOAuth2AuthorizationRequestResolver(clientRegistrationRepository, base_uri);
+        resolver.setAuthorizationRequestCustomizer(OAuth2AuthorizationRequestCustomizers.withPkce());
+
         http
                 //所有请求都需经过授权认证
                 .authorizeHttpRequests(authorize -> authorize
                         .anyRequest().authenticated())
                 //配置登录URL
-                .oauth2Login(oauth2Login ->
-                        oauth2Login.loginPage("/oauth2/authorization/myoauth2"))
+                .oauth2Login(oauth2Login ->{
+                    oauth2Login.loginPage("/oauth2/authorization/myoauth2");
+                    oauth2Login.authorizationEndpoint().authorizationRequestResolver(resolver);
+                    oauth2Login.userInfoEndpoint(userInfo -> userInfo
+                            .oidcUserService(this.oidcUserService()));
+                })
+
                 //使用默认客户端配置
                 .oauth2Client(withDefaults());
         return http.build();
+    }
+    private OAuth2UserService<OidcUserRequest, OidcUser> oidcUserService() {
+        final OidcUserService delegate = new OidcUserService();
+
+        return (userRequest) -> {
+            OidcUser oidcUser = delegate.loadUser(userRequest);
+            OAuth2AccessToken accessToken = userRequest.getAccessToken();
+            Set<GrantedAuthority> mappedAuthorities = new HashSet<>();
+            try {
+                JWT jwt = JWTParser.parse(accessToken.getTokenValue());
+                JWTClaimsSet claimSet = jwt.getJWTClaimsSet();
+                Collection<String> userAuthorities = claimSet.getStringListClaim("authorities");
+                mappedAuthorities.addAll(userAuthorities.stream()
+                        .map(SimpleGrantedAuthority::new)
+                        .toList());
+            } catch (ParseException e) {
+                System.err.println("Error OAuth2UserService: " + e.getMessage());
+            }
+            oidcUser = new DefaultOidcUser(mappedAuthorities, oidcUser.getIdToken(), oidcUser.getUserInfo());
+            return oidcUser;
+        };
     }
 }
