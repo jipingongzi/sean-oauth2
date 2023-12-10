e9ff05189f9369c4accc1afe8ce4ab357c875b01
sean xiao <435032183@qq.com>
2023-12-10 16:45:00 +0800

    debug

--- authorization-server/src/main/java/org/example/AuthorizationServer.java
+++ authorization-server/src/main/java/org/example/AuthorizationServer.java
@@ -2,10 +2,16 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class AuthorizationServer {
     public static void main(String[] args) {
-        SpringApplication.run(AuthorizationServer.class, args);
+        ApplicationContext context = SpringApplication.run(AuthorizationServer.class, args);
+        String[] names =  context.getBeanDefinitionNames();
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
+        System.out.println(1);
     }
 }
