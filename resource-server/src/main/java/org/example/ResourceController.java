53c62075ae6c8228cc1c3836ec2ee3b6f217367f
sean xiao <Sean_Xiao@epam.com>
2023-12-14 21:55:06 +0800

    optimize code

--- resource-server/src/main/java/org/example/ResourceController.java
+++ resource-server/src/main/java/org/example/ResourceController.java
@@ -2,7 +2,8 @@ package org.example;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 
