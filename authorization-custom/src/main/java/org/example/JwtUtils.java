0a4f74de691dd863192c846b63e7e3d42409b8b6
sean xiao <Sean_Xiao@epam.com>
2023-12-14 21:42:25 +0800

    optimize code

--- authorization-custom/src/main/java/org/example/JwtUtils.java
+++ authorization-custom/src/main/java/org/example/JwtUtils.java
@@ -19,29 +19,29 @@ public class JwtUtils {
     /**
      * 从JWT中提取用户名
      */
-    public String extractUsername(String token){
-        return extractClaim(token,Claims::getSubject);
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
     }
 
     /**
      * 从JWT中提取过期时间
      */
-    public Date extractExpiration(String token){
-        return extractClaim(token,Claims::getExpiration);
+    public Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
     }
 
     /**
      * 检查JWT是否包含指定的声明
      */
-    public boolean hasClaim(String token,String claimName){
+    public boolean hasClaim(String token, String claimName) {
         final Claims claims = extractAllClaims(token);
-        return claims.get(claimName) !=null;
+        return claims.get(claimName) != null;
     }
 
     /**
      * 从JWT中提取指定声明
      */
-    public <T> T extractClaim(String token, Function<Claims,T> claimsResolver){
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
         final Claims claims = extractAllClaims(token);
         return claimsResolver.apply(claims);
     }
@@ -49,7 +49,7 @@ public class JwtUtils {
     /**
      * 从JWT中提取所有声明
      */
-    public Claims extractAllClaims(String token){
+    public Claims extractAllClaims(String token) {
         try {
             return Jwts.parser().setSigningKey(jwtSigningKey).parseClaimsJws(token).getBody();
         } catch (Exception e) {
@@ -62,7 +62,7 @@ public class JwtUtils {
     /**
      * 检查JWT是否已过期
      */
-    public Boolean isTokenExpired(String token){
+    public Boolean isTokenExpired(String token) {
         Date expirationDate = extractExpiration(token);
         if (expirationDate == null) {
             return true; // or false based on your requirements
@@ -73,34 +73,34 @@ public class JwtUtils {
     /**
      * 生成JWT
      */
-    public String generateToken(UserDetails userDetails){
-        Map<String,Object> claims = new HashMap<>();
-        return createToken(claims,userDetails);
+    public String generateToken(UserDetails userDetails) {
+        Map<String, Object> claims = new HashMap<>();
+        return createToken(claims, userDetails);
     }
 
     /**
      * 生成带有指定声明的JWT
      */
-    public String generateToken(UserDetails userDetails,Map<String,Object>claims){
-        return createToken(claims,userDetails);
+    public String generateToken(UserDetails userDetails, Map<String, Object> claims) {
+        return createToken(claims, userDetails);
     }
 
     /**
      * 创建JWT
      */
-    public String createToken(Map<String, Object> claims, UserDetails userDetails){
+    public String createToken(Map<String, Object> claims, UserDetails userDetails) {
         return Jwts.builder().setClaims(claims)
                 .setSubject(userDetails.getUsername())
-                .claim("authorities",userDetails.getAuthorities())
+                .claim("authorities", userDetails.getAuthorities())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + TimeUnit.HOURS.toMillis(24)))
-                .signWith(SignatureAlgorithm.HS256,jwtSigningKey).compact();
+                .signWith(SignatureAlgorithm.HS256, jwtSigningKey).compact();
     }
 
     /**
      * 验证JWT是否有效
      */
-    public Boolean isTokenValid(String token,UserDetails userDetails){
+    public Boolean isTokenValid(String token, UserDetails userDetails) {
         final String username = extractUsername(token);
         return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
     }
